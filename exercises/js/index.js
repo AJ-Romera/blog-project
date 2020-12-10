ef2649e09b06108b2ed86072605dcfe84a1387a3
AJ-Romera <ajromeraalfonso@gmail.com>
2020-12-10 18:49:04 +0100

    Finish Blog App

--- exercises/js/index.js
+++ exercises/js/index.js
@@ -10,31 +10,35 @@ window.onload = () => {
 const getPosts = () => {
     fetch(API_URL, {
         method: 'GET'
-    }).then((response) => {
+    }).then((response)=>{
         return response.json();
-    }).then((data) => {
+    }).then((data)=>{
         buildPosts(data);
     })
 }
 
 const buildPosts = (blogPosts) => {
-    let blogPostsContent = '';
-    for (blogPost of blogPosts) {
+    let blogPostsContent = "";
+    for(blogPost of blogPosts){
+        const postImage = API_BASE_URL + blogPost.post_image;
+        const postLink = `/post.html?id=${blogPost.id}`;
         const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
-        const postImage = `${API_BASE_URL}${blogPost.post_image}`
-        const postLink = `post.html?id=${blogPost.id}`;
         blogPostsContent += `
-        <a class="post-link" href="${postLink}">
-            <div class="post">
-                <div class="post-image" style="background-image: url(${postImage})"></div>
-                <div class="post-content">
-                    <div class="post-date">${postDate}</div>
-                    <div class="post-title"><h4>${blogPost.title}</h4></div>
-                    <div class="post-text">${blogPost.content}</div>
+            <a class="post-link" href="${postLink}">
+                <div class="post">
+                    <div class="post-image" style="background-image: url(${postImage})"></div>
+                    <div class="post-content">
+                        <div class="post-date">${postDate}</div>
+                        <div class="post-title">
+                            <h4>${blogPost.title}</h4>
+                        </div>
+                        <div class="post-text">
+                            <p>${blogPost.content}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
         `
     }
-    document.querySelector('.blog-posts').innerHTML = blogPostsContent;
+    document.querySelector(".blog-posts").innerHTML = blogPostsContent;
 }
--- exercises/js/post.js
+++ exercises/js/post.js
@@ -1,9 +1,3 @@
-/**
- * TODO (Together): Create getPostIdParam to get the id of the post to use in the request later
- * TODO: Complete getPost function to get post data from API
- * TODO: Complete buildPost function to fill in the post data in the post.html file using ids
- */
-
 const API_URL = "http://localhost:3000/api/posts/";
 const API_BASE_URL = "http://localhost:3000/";
 
@@ -18,24 +12,22 @@ const getPostIdParam = () => {
 }
 
 const getPost = () => {
-    // CODE GOES HERE
-    const postId = getPostIdParam();
-    const url = `${API_URL}${postId}`;
+    const url = `${API_URL}${getPostIdParam()}`
     fetch(url, {
         method: 'GET'
-    }).then((response) => {
+    }).then((response)=>{
         return response.json();
-    }).then((data) => {
-        buildPosts(data);
+    }).then((data)=>{
+        buildPost(data);
     })
 }
 
-const buildPosts = (data) => {
+const buildPost = (data) => {
+    console.log(data);
     const postDate = new Date(parseInt(data.added_date)).toDateString();
-    const postImage = `${API_BASE_URL}${data.post_image}`;
-    document.querySelector('header').style.backgroundImage = `url(${postImage})`;
-    document.getElementById('individual-post-title').innerText = data.title;
-    document.getElementById('individual-post-date').innerText = `Published on ${postDate}`;
-    document.getElementById('individual-post-content').innerText = data.content;
+    const postImage = API_BASE_URL + data.post_image;
+    document.querySelector("header").style.backgroundImage = `url(${postImage})`;
+    document.getElementById("individual-post-title").innerText = data.title;
+    document.getElementById("individual-post-date").innerText =  `Published on ${postDate}`;
+    document.getElementById("individual-post-content").innerText = data.content;
 }
-
